dfb93df44f822c73dbf627ace21e519c5cdb8988
Lasse Aakjær <51378679+TheLoGgI@users.noreply.github.com>
2020-10-28 00:43:21 +0100

    Prograsstions bar

--- js/main.js
+++ js/main.js
@@ -75,9 +75,41 @@ navgationSelectionHandler('headernav')
 window.addEventListener('resize', () => moveSelection())
 
 
+// Progress Bar
+const waypoints = document.querySelectorAll('.waypoint')
+let progresionLevel = 0
+// document.getElementById('progress').setAttribute('value', '0%')
+document.getElementById('next').addEventListener('click', e => {
+    const barProgres = document.getElementById('progress')
+    const currentValue = Number(barProgres.getAttribute('value')) + 5
+
+    if (currentValue <= 100) {
+        document.getElementById('progress').style.width = `${currentValue}%`
+        barProgres.setAttribute('value', currentValue)
+        console.log(currentValue % 25);
+    
+        if (currentValue % 25 === 0) {
+            progresionLevel++
+            activateWaypoint(waypoints[progresionLevel])
+            console.log(currentValue);
+        }
+
+    } else {
+        return
+    }
+
+    
+})
+
+function activateWaypoint(waypoint) {
+    waypoint.classList.toggle('waypoint-done')
+    waypoint.children[1].style.stroke = 'white'
+    
+}
 
 // function classToggle(elementID, className) {
 //     document.getElementById(elementID)
 //         .classList.toggle(className)
 //         return document.getElementById(elementID)
 // }
+
