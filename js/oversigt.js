dd82ce798638bc1f377c1affcce348a21c2d5a00
Emil Bruun Larsen <60384288+Emilovich98@users.noreply.github.com>
2020-11-08 21:34:25 +0100

    Små oversigter klar til chart.js
    Alle små oversigter stylet, og klar til chart.js

--- js/oversigt.js
+++ js/oversigt.js
@@ -49,14 +49,117 @@ function skjulEmner() {
     const emneBox = document.querySelector(".graf-element1");
     emneBox.classList.toggle("emne-hidden");
     console.log(this);
-   
-    if (emneNavn.textContent === "Vis emner"){
+
+    if (emneNavn.textContent === "Vis emner") {
         emneNavn.textContent = "Skjul emner"
-        
+
     }
-else {
-     emneNavn.textContent = "Vis emner"
-    
-}
+    else {
+        emneNavn.textContent = "Vis emner"
+
+    }
 
 }
+
+
+//Small overviews - Small graphs
+
+//array for power usage and production
+// Array of objects colors
+let _colorData = [{
+    dataType: "Produceret strøm",
+    amountOfPower: 248,
+    backgroundColor: "rgba(255, 99, 132, 0.2)",
+    borderColor: "rgba(255, 99, 132, 1)"
+}, {
+    dataType: "Købt strøm",
+    amountOfPower: 348,
+    backgroundColor: "rgba(54, 162, 235, 0.2)",
+    borderColor: "rgba(54, 162, 235, 1)"
+}];
+
+// 2: prepare data for chart
+function prepareData(data) {
+    let dataType = [];
+    let amountOfPower = [];
+    let backgroundColors = [];
+    let borderColors = [];
+
+    for (const dataObject of data) {
+        dataType.push(dataObject.dataType);
+        amountOfPower.push(dataObject.amountOfPower);
+        backgroundColors.push(dataObject.backgroundColor);
+        borderColors.push(dataObject.borderColor);
+    }
+
+    return {
+        dataType,
+        amountOfPower,
+        backgroundColors,
+        borderColors
+    };
+}
+
+// 3: create and append the chart
+function appendChart(data) {
+    // create a new chart
+    let chart = new Chart(document.getElementById('power-graph'), {
+        type: 'bar',
+        data: {
+            labels: data.dataType,
+            datasets: [{
+                label: 'Strømforbrug',
+                data: data.amountOfPower,
+                backgroundColor: data.backgroundColors,
+                borderColor: data.borderColors,
+                borderWidth: 1
+            }]
+        },
+        // Options and configuration options goes here
+        // Go to the docs to find more: https://www.chartjs.org/docs/latest/
+        // Bar chart: https://www.chartjs.org/docs/latest/charts/bar.html 
+        options: {
+            backgroundColor: 'black',
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        callback: function (value, index, values) {
+                            return ''
+                        },
+                        max: 30,
+                        min: 0
+                    },
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        beginAtZero: false,
+
+                    },
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                scaleLabel: {
+                    display: false,
+                    fontSize: 16
+                },
+            },
+            layout: {
+                padding: 0
+            },
+            legend: {
+                display: false,
+                position: 'top',
+                labels: {
+                    fontColor: "black",
+                }
+            }
+        }
+    });
+}
+let data = prepareData(_colorData);
+appendChart(data);
